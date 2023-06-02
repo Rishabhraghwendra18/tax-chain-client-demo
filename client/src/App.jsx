7b9c6d44dd2283d8c51d68c0b042f005ab1067aa
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-06-03 00:01:47 +0530

    fixed react routing issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,35 +8,18 @@ import {
 import Home from "./pages/home/Home";
 import Citizen from "./pages/citizen/Citizen";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Home />}>
-      <Route index path="/citizen" element={<Citizen />} />
-      {/* <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} /> */}
-    </Route>
-  )
-);
-
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/citizen',
+    element:<Citizen/>
+  }
+])
 export default function App() {
   return (
-    <div className="container">
-      {/* <Renderer /> */}
-      {/* <Navigation></Navigation> */}
       <RouterProvider router={router} />
-    </div>
   );
 }
-// function Renderer(params) {
-//   return (
-//     <Router>
-//       <div className="app">
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           {/* <Route path="/about" element={<About />} /> */}
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
