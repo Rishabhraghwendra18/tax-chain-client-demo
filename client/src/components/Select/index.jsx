b46e721f3731d18e668f620b3bae44bbdcd53870
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2023-06-07 17:21:54 +0530

    made changes in contractor

--- client/src/components/Select/index.jsx
+++ client/src/components/Select/index.jsx
@@ -4,22 +4,21 @@ import './index.css';
 
 function Select({onChange,children,placeholder,styles,disabled}) {
   return (
-    <Form.Select
-    // as="select"
+    <Form.Control
+    as="select"
     style={{
       background: 'black',
       color: 'white',
       border: '1px solid lightgrey',
       ...styles
     }}
-    custom
     onChange={onChange}
     className="custom-select"
     placeholder={placeholder}
     disabled={disabled}
     >
         {children}
-    </Form.Select>
+    </Form.Control>
   )
 }
 
--- client/src/pages/contractor/Contractor.jsx
+++ client/src/pages/contractor/Contractor.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Navigation from "../../components/navigation/Navbar";
+import {
+  useSDK,
+  useContractRead,
+  useContract,
+  useAddress,
+  useDisconnect,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+import { bigNumberToEthers } from "../../utils/bigNumberToEther";
+import Select from "../../components/Select/index";
 import "./contractor.css";
 import {
   Container,
@@ -14,15 +24,77 @@ import Footer from "../../components/footer/Footer.jsx";
 import Header from "../../components/header/Header.jsx";
 import GenerateContract from "../../components/generateContract/GenerateContract.jsx";
 import ContractTable from "../../components/Tables/ContractTable.jsx";
+import WETHABI from "../../contractsABI/WETH.json";
+import CONTRACTORABI from "../../contractsABI/Contractor.json";
+
+const WETH_ADDRESS = "0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9";
+const CONTRACTOR_REGISTERY= import.meta.env.VITE_CONTRACTOR_ADDRESS;
 
 function Contractor() {
+  const sdk = useSDK();
+  const disconnect= useDisconnect();
+  const [contractors, setContractors] = useState([]);
+  const [selectedContractor, setSelectedContractor] = useState('');
+  const [contractorBalance, setContractorBalance] = useState('');
+
+  useEffect(()=>{
+    getAllRegisteries()
+    return disconnect;
+  },[])
+useEffect(()=>{
+  if(!selectedContractor)return;
+  getContractorBalance();
+},[selectedContractor])
+const getAllRegisteries = async ()=>{
+  const Contract = await sdk.getContract(CONTRACTOR_REGISTERY,CONTRACTORABI);
+  const data = await Contract.call(
+    "getAllRegisteries"
+  );
+  if(data.length>0){
+    setContractors(data);
+    setSelectedContractor(data[0]);
+  }
+}
+const getContractorBalance = async ()=>{
+  const Contract = await sdk.getContract(WETH_ADDRESS,WETHABI);
+  const data = await Contract.call(
+    "balanceOf",
+    [selectedContractor]
+  );
+    setContractorBalance(bigNumberToEthers(data));
+}
+const handleChangeContractor = (event)=>{
+  console.log("d",event.target.value);
+}
   return (
     <div>
       <Navigation></Navigation>
-      <Header heading="Contractor: Rahul Sharma"></Header>
+      <Header heading={`Contractor: ${selectedContractor}`}>
+      <div className="d-flex justify-content-center align-items-center">
+
+      <Select
+      onChange={handleChangeContractor}
+    >
+          {/* {userConstituencyIsLoading ?<option value={"Searching..."}>{"Searching..."}</option>:constituencyContract?.map((e,index)=>(
+          <option value={e} key={index}>{e}</option>
+        ))} */}
+          {contractors?.map((e, index) => (
+            <option value={e} key={index}>
+              {e}
+            </option>
+          ))}
+        </Select>
+      </div>
+      </Header>
       <Container>
         <Row>
-          <GenerateContract web3={web3}></GenerateContract>
+          {/* <GenerateContract web3={web3}></GenerateContract> */}
+          <h1
+          style={{
+            marginTop:'1rem',
+            textAlign:'center'
+          }}
+          >Contractor Current Balance: {contractorBalance} WETH</h1>
           <Col md={12}>
             <Card className="table-card">
               <Card.Body>
